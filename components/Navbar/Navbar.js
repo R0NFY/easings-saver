cda3b8b974d97db2370953bd8fcb162d331b728b
r0nfy <r0nfymain@gmail.com>
2022-03-01 20:45:17 +0700

    to set up the project

---
+++ components/Navbar/Navbar.js
@@ -0,0 +1,18 @@
+import styles from './Navbar.module.scss'
+import Image from 'next/image';
+import React from 'react';
+
+
+const Navbar = () => {
+    return (
+        <div className={styles.container}>
+            <a href="#">Log in</a>
+            <a href="#" className={styles.google}>
+                <Image className='logo' src="/google_logo.svg" alt="google logo" height={14} width={14} />
+                <p>Sign up with Google</p>
+            </a>
+        </div>
+    );
+}
+
+export default Navbar;
