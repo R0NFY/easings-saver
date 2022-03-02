e8fe75988a2525fa7841abb67d0a385c3d9890ed
r0nfy <r0nfymain@gmail.com>
2022-03-02 20:31:17 +0700

    To add image to main page

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,9 +1,14 @@
 import styles from './Layout.module.scss'
+import Image from 'next/image';
 
 export const Layout = () => {
     return (
         <div className={styles.container}>
-        <h1>Don&apos;t lose your precious <span>easings</span> ever again</h1>
+            <h1>Don&apos;t lose your precious <span>easings</span> ever again</h1>
+            <div className={styles.image}>
+                <Image src='/illustration.svg' alt='Bench and trees' width={478} height={260} />
+            </div>
+            <h2>Add your first easing</h2>
         </div>
     );
   }
--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -7,7 +7,7 @@ export const NavBar = () => {
         <div className={styles.container}>
             <a href="#">Log in</a>
             <a href="#" className={styles.google}>
-                <Image className='logo' src="/google_logo.svg" alt="google logo" height={14} width={14} />
+                <Image src="/google_logo.svg" alt="google logo" height={14} width={14} />
                 <p>Sign up with Google</p>
             </a>
         </div>
